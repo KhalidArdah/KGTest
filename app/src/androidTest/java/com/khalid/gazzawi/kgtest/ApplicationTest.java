6401920df5c1b4bf8e04960fbef2fb0acfc96ddb
KhalidArdah <khalid.jamal.4002@gmail.com>
2016-08-07 00:59:35 +0300

    Empty Project
    the first creation of the project

---
+++ app/src/androidTest/java/com/khalid/gazzawi/kgtest/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.khalid.gazzawi.kgtest;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
