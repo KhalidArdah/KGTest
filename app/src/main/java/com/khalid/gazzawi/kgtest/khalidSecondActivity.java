962cf70036fd159973fda029c7ff35dbd535f9f4
KhalidArdah <khalid.jamal.4002@gmail.com>
2016-08-19 13:26:31 +0300

    LAST

--- app/src/main/java/com/khalid/gazzawi/kgtest/khalidSecondActivity.java
+++ app/src/main/java/com/khalid/gazzawi/kgtest/khalidSecondActivity.java
@@ -3,6 +3,8 @@ package com.khalid.gazzawi.kgtest;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class khalidSecondActivity extends AppCompatActivity {
@@ -10,12 +12,26 @@ public class khalidSecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_khalid_second);
-        Intent intent=getIntent();
+        setContentView(R.layout.activity_khalid_second);
+        final Intent intent=getIntent();
         String message=intent.getStringExtra(MESSAGE_KEY);
-        TextView textView=new TextView(this);
+        TextView textView=(TextView) findViewById(R.id.textView);
         textView.setTextSize(45);
         textView.setText(message);
-        setContentView(textView);
+        Button bn=(Button)findViewById(R.id.nxt);
+        final Intent intent1=new Intent(this,KThirdActivity.class);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent1);
+            }
+        });
+
     }
+    public void mm(View view){
+        Intent i =new Intent(this,KThirdActivity.class);
+        startActivity(i);
+
+    }
+
 }
